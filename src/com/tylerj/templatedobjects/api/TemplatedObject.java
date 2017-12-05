1701f82ee6fa72ad15c1697cd7202ac5fced60ec
Tyler Jaacks <tylerjaacks@gmail.com>
2017-12-04 21:19:02 -0600

    Added project and initial api implementation.

---
+++ src/com/tylerj/templatedobjects/api/TemplatedObject.java
@@ -0,0 +1,13 @@
+package com.tylerj.templatedobjects.api;
+
+import java.util.HashMap;
+
+public class TemplatedObject {
+    private String objectName;
+    private HashMap<String, String[]> objectFields;
+
+    public TemplatedObject(String objectName, HashMap<String, String[]> objectFields) {
+        this.objectName = objectName;
+        this.objectFields = objectFields;
+    }
+}
---
+++ src/com/tylerj/templatedobjects/codegen/Main.java
@@ -0,0 +1,8 @@
+package com.tylerj.templatedobjects.codegen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
