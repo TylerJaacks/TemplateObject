cdd222433764d9ef0d699483893d8c1ebb9c1a8a
Tyler Jaacks <tylerjaacks@gmail.com>
2017-12-05 17:37:21 -0600

    Started working on the CallParser class.

--- src/main/java/com/tylerj/templatedobjects/codegen/ClassParser.java
+++ src/main/java/com/tylerj/templatedobjects/codegen/ClassParser.java
@@ -1,10 +1,30 @@
 package com.tylerj.templatedobjects.codegen;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+/**
+ * ClassParser parses Java classes and finds TemplatedObject code
+ * and creates a model of the projected class.
+ */
 public class ClassParser {
-    public static ClassModel parseClass(File file) {
+    private static BufferedReader bufferedReader;
 
+    /**
+     * Parses a given Java file and finds the TemplatedObject call.
+     *
+     * @param filePath path to a given Java file to be parsed.
+     * @return a ClassModel representing the TemplatedObject.
+     */
+    protected static ClassModel parseClass(String filePath) {
+        try {
+            BufferedReader bufferedReader = bufferedReader = new BufferedReader(new FileReader(filePath));
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be found check to make sure the file exists.");
+        }
 
         return null;
     }
